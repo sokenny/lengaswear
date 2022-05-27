f2e77d4786c4fdd0e4510801bcb041843500ba87
sokenny <juanchaher99@gmail.com>
2022-05-27 20:11:14 -0300

    animate ProductCategory page

--- components/layouts/ProductCategory/ProductCategory.tsx
+++ components/layouts/ProductCategory/ProductCategory.tsx
@@ -1,4 +1,5 @@
 import { ProductType } from 'types';
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './ProductCategory.module.scss';
@@ -9,20 +10,32 @@ type ProductCategoryProps = {
     products: ProductType[]
 }
 
+const motionProps = {
+    initial: {opacity: 0, y: 20},
+    animate: {opacity: 1, y: 0},
+    transition: {duration: 1, ease: 'easeOut'}
+}
+
 const ProductCategory:React.FC<ProductCategoryProps> = ({title, description, products}) => {
     return (
         <div className={styles.ProductCategory}>
             <div className={styles.container}>
                 <main>
                     <div className={styles.header}>
-                        <h1>{title}</h1>
-                        <h2>{description}</h2>
+                        <motion.h1 {...motionProps} >{title}</motion.h1>
+                        <motion.h2 {...motionProps} transition={{duration: 1.2, delay: 0, ease: 'easeOut'}} >{description}</motion.h2>
                     </div>
-                    <section className={styles.products}>
-                        {products.map((product)=>
-                            <div key={product.name}>
+                    <section 
+                    className={styles.products}
+                    >
+                        {products.map((product, i)=>
+                            <motion.div 
+                            key={product.name}
+                            {...motionProps}
+                            transition={{duration: 1.2, delay: (i) * .1, ease: 'easeOut'}}
+                            >
                                 <ProductItem product={product} />
-                            </div>
+                            </motion.div>
                         )}
                     </section>
                 </main>
