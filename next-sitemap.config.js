3dc43f04abc2984ce01354d793645bfe87a7613e
sokenny <juanchaher99@gmail.com>
2022-06-27 21:11:18 -0300

    fix sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
+
 const config = {
     siteUrl: process.env.NEXT_PUBLIC_APP_URL || 'https://lengaswear.com',
     generateRobotsTxt: true,
     exclude: ['/gracias'],
-  }
+}
   
-  export default config
+module.exports = config
