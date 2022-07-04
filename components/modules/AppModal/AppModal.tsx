98557d997241c9fcd4304c8ef7e426990e15041f
sokenny <juanchaher99@gmail.com>
2022-07-04 10:27:12 -0300

    remove innecessary log

--- components/modules/AppModal/AppModal.tsx
+++ components/modules/AppModal/AppModal.tsx
@@ -15,7 +15,7 @@ const AppModal:React.FC = () => {
         animate={{opacity: 1}}
         exit={{opacity: 0}}
         transition={{duration: .1}}
-        onClick={()=>{setModal(false); console.log('hola')}}
+        onClick={()=>setModal(false)}
         >
             <motion.div
             initial={{opacity: 0, scale: .8}}
